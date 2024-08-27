92a52b99fb673f871374e6f7c403e6e59c9892b9
Angel <24.apbusiness@gmail.com>
2024-08-26 19:45:22 -0700

    I added LinkedList notes

--- Reference/src/Practice.java
+++ Reference/src/Practice.java
@@ -1,20 +1,10 @@
 import java.util.*;
 
 public class Practice {
+
     public static void main(String[] args) {
-        int endNum = 10;
 
-        beginEnd(endNum);
-    }
-
-    static void beginEnd(int endNum) {
-        List<Integer> newList = new ArrayList<Integer>();
-
-        for (int i = 1; i < endNum; i++){
-            newList.add(i);
-            System.out.println(newList);
-        }
-        System.out.println(newList.get(10));
 
     }
+
 }
